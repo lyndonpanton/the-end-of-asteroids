3a63cd6f9a9c523dd231552692113c409b3994d8
Lyndon Panton <LyndonPanton@gmail.com>
2023-02-14 02:50:16 +0000

    Added score to UI
    Score increases by 1 approximately every second

---
+++ Asteroids/Assets/Scripts/HUD.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.Profiling;
+using UnityEngine;
+
+/// <summary>
+/// A canvas which displays the current score of the canvas
+/// </summary>
+public class HUD : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI textMeshProUGUI;
+
+    float timeElapsed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMeshProUGUI.text = "0";
+        timeElapsed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        textMeshProUGUI.text = ((int)timeElapsed).ToString();
+    }
+}
